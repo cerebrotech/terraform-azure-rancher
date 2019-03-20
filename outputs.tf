d223a1a576e656eb9a9b25ca250567150565975c
Steven Davidovitz <steven.davidovitz@dominodatalab.com>
2019-03-20 10:07:40 -0700

    add output for cluster provisioning so we can `depends_on` it

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,8 @@ output "application_security_group_id" {
   description = "ID of the ASG attached to all instance NICs"
   value       = "${azurerm_application_security_group.this.id}"
 }
+
+output "cluster_provisioned" {
+  description = "ID of the null_resource cluster provisioner"
+  value       = "${null_resource.provision_cluster.id}"
+}
